722fa5e2684605aab344d6a8c2f20508640b2379
Matheus Felipe V. de Freitas <sql1freitas@gmail.com>
2025-06-02 11:05:33 -0300

    admin editado com filtros e pesquisa

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,46 @@
 from django.contrib import admin
+from .models import Employee, Meal
 
-# Register your models here.
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'registration_id',
+        'department',
+        'active',
+        'register_date'
+    )
+
+    search_fields = (
+        'name',
+        'registration_id',
+    )
+
+    list_filter = (
+        'department',
+        'active',
+    )
+
+    ordering = (
+        'name',
+    )
+
+@admin.register(Meal)
+class MealAdmin(admin.ModelAdmin):
+    list_display = (
+        'employee',
+        'meal_date',
+        'meal_time',
+    )
+
+    search_fields = (
+        'employee_name',
+        'employee_registration_id',
+    )
+
+    list_filter = (
+        'meal_date',
+        'meal_time',
+    )
+
+    date_hierarchy = 'meal_date'
