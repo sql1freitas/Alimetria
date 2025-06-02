28c3317ba57789d32f0b51d63cd8db5da511c8b7
Matheus Felipe V. de Freitas <sql1freitas@gmail.com>
2025-06-02 11:39:03 -0300

    Criação de testes

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
 
-# Create your tests here.
+from django.test import TestCase
+from django.utils import timezone
+from .models import Employee, Meal
+
+class MealTestCase(TestCase):
+
+    def setUp(self):
+        self.emp = Employee.objects.create(
+            name="João Teste",
+            registration_id="12345"
+        )
+
+    def test_meal_registration(self):
+        meal = Meal.objects.create(employee=self.emp)
+        self.assertEqual(meal.employee.name, "João Teste")
+
+    def test_unique_meal_per_day(self):
+        Meal.objects.create(employee=self.emp, meal_date=timezone.now().date())
+        with self.assertRaises(Exception):
+            Meal.objects.create(employee=self.emp, meal_date=timezone.now().date())
+
