4b262dc776a02cb792e21036276893f663689759
Matheus Felipe V. de Freitas <sql1freitas@gmail.com>
2025-06-02 10:49:26 -0300

    Implementação do front básico

--- alimetria/urls.py
+++ alimetria/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from core.views import register_meal
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('registro/', register_meal, name='register_meal'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.db import IntegrityError
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .models import Meal, Employee
 
-# Create your views here.
+def register_meal(request):
+    if request.method == 'POST':
+        reg_id = request.POST.get('registration_id')
+        try:
+            employee = Employee.objects.get(registration_id=reg_id, active=True)
+        except Employee.DoesNotExist:
+            messages.error(request, "Matrícula não encontrada ou funcionário inativo.")
+            return redirect('register_meal')
+
+        try:
+            Meal.objects.create(employee=employee)
+            messages.success(request, "Refeição registrada com sucesso!")
+        except IntegrityError:
+            messages.warning(request, "Este funcionário já registrou refeição hoje.")
+        
+        return redirect('register_meal')
+
+    return render(request, 'register_meal.html')
+
